1510539f49f2625adf19cb391197341e9a02ab02
mccrush <mccrush2027@gmail.com>
2023-12-18 18:49:19 +0700

    Settings work adaptive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 window.onload = () => {
   console.log('Hi')
 
-  const totop = document.querySelector("#totop");
-  window.addEventListener("scroll", (e) => { })
+  let btnToggle = document.querySelector("#btnToggle");
+  let mobItems = document.querySelector("#mobItems");
+
+  btnToggle.addEventListener("click", (e) => {
+    // if (mobItems.classList(showItems)) {
+
+    // }
+    console.log('Hi')
+    mobItems.classList.toggle('showItems')
+  })
 }
